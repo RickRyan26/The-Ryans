6f6d8062222ff8a4f029068f1703d3beabcdd3d6
Rick Ryan <72894373+RickRyan26@users.noreply.github.com>
2024-03-13 11:44:05 -0700

    ss

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,7 +2,11 @@
 import { config } from '$lib/config';
 
 export async function load({ url }) {
-	const id = url.searchParams.get('id') || 'hilltop';
+	const id = url.searchParams.get('id')
+		? url.searchParams.get('id')
+		: url.origin === 'https://www.debrakaygeorgeinteriors.com'
+			? 'debra'
+			: 'hilltop';
 
 	// if (!id) {
 	// 	error(404, {
